5f6df2c9ff088098955e6f87be77f0571a693cf7
manterfield <tom.manterfield@11fs.com>
2019-09-09 14:54:43 +0100

    [GEN6-759] Updated bash script paths to properly refer to the root of the eck project

--- eck/.concourse/scripts/deploy.sh
+++ eck/.concourse/scripts/deploy.sh
@@ -2,5 +2,5 @@
 set -e
 set -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ECK_ROOT="$(dirname "${DIR}")"
+ECK_ROOT="$(dirname $(dirname "${DIR}"))"
 kubectl apply -k "${ECK_ROOT}/k8s"
