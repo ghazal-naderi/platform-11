1f6efb088fdf5c585a778c616c434e97236e5d00
manterfield <tom.manterfield@11fs.com>
2019-09-12 18:52:56 +0100

    [GEN6-759] Added secret with auth details for elasticsearch

--- fluentd/.concourse/scripts/deploy.sh
+++ fluentd/.concourse/scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 set -o pipefail
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ECK_ROOT="$(dirname $(dirname "${DIR}"))"
-kubectl apply -k "${ECK_ROOT}/k8s"
+FLUENTD_ROOT="$(dirname $(dirname "${DIR}"))"
+kubectl apply -k "${FLUENTD_ROOT}/k8s"
