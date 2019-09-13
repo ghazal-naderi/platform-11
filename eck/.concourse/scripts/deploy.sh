7be1baf45596899934e9f195359a1d2cff0541ed
manterfield <tom.manterfield@11fs.com>
2019-09-13 11:20:29 +0100

    [GEN6-759] Updated to make eck emit secrets into kube-system namespace and fluentd to consume them

--- eck/.concourse/scripts/deploy.sh
+++ eck/.concourse/scripts/deploy.sh
@@ -4,3 +4,7 @@ set -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ECK_ROOT="$(dirname $(dirname "${DIR}"))"
 kubectl apply -k "${ECK_ROOT}/k8s"
+
+# After deploy we need to export the cert and user credential secrets into kube-system for use by other services
+kubectl -n elasticstack get secret elasticsearch-es-http-certs-public --export -o yaml | kubectl apply -n=kube-system -f -
+kubectl -n elasticstack get secret elasticsearch-es-elastic-user --export -o yaml | kubectl apply -n=kube-system -f -
--- fluentd/.concourse/scripts/deploy.sh
+++ fluentd/.concourse/scripts/deploy.sh
@@ -2,13 +2,6 @@
 set -e
 set -o pipefail
 
-# !!!!!!!!! TODO: Just copy the secret from the eck deploy and put it into kube-system !!!!!!!!!!!!!!
-# Generate a random password and add along with fluentd user to file elasticsecret.env
-# Move/ensure file is located alongisde k8s/kustomization.yaml
-# Create new user in elasticsearch or update existing user
-#   - is the only option to use the API?
-#   - how could we add a user via file auth?
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 FLUENTD_ROOT="$(dirname $(dirname "${DIR}"))"
 kubectl apply -k "${FLUENTD_ROOT}/k8s"
