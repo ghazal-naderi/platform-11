5f3a6297b18d0d29ff10b285141da084bc3133b9
manterfield <tom.manterfield@11fs.com>
2019-09-17 11:57:32 +0100

    [GEN6-759] Updated namespace and naming scheme for all eck resources

--- eck/.concourse/scripts/deploy.sh
+++ eck/.concourse/scripts/deploy.sh
@@ -6,5 +6,5 @@ ECK_ROOT="$(dirname $(dirname "${DIR}"))"
 kubectl apply -k "${ECK_ROOT}/k8s"
 
 # After deploy we need to export the cert and user credential secrets into kube-system for use by other services
-kubectl -n elasticstack get secret elasticsearch-es-http-certs-public --export -o yaml | kubectl apply -n=kube-system -f -
-kubectl -n elasticstack get secret elasticsearch-es-elastic-user --export -o yaml | kubectl apply -n=kube-system -f -
+kubectl -n eck get secret eck-es-http-certs-public --export -o yaml | kubectl apply -n=kube-system -f -
+kubectl -n eck get secret eck-es-elastic-user --export -o yaml | kubectl apply -n=kube-system -f -
