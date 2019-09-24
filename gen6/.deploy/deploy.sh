a019ce83a574cd869cd7b6c3d7f3521e713cceb3
manterfield <tom.manterfield@11fs.com>
2019-09-24 16:18:00 +0100

    [GEN6-807] Removed some debugging info

--- gen6/.deploy/deploy.sh
+++ gen6/.deploy/deploy.sh
@@ -10,11 +10,10 @@ kubectl apply -k "${K8S_DIR}"
 kubectl -n eck get secret eck-es-http-certs-public --export -o yaml | kubectl apply -n=kube-system -f -
 kubectl -n eck get secret eck-es-elastic-user --export -o yaml | kubectl apply -n=kube-system -f -
 
+# We use the values in the es secrets to cause a rollingdeploy of fluentd if changed.
 CERT=$(kubectl -n eck get secret "eck-es-http-certs-public" -o go-template='{{index .data "tls.crt"}}')
 PW=$(kubectl -n eck get secret "eck-es-elastic-user" -o go-template='{{.data.elastic}}')
-
 ES_SECRET_HASH=($( echo -n ${CERT}${PW} | md5sum))
-echo $ES_SECRET_HASH
 
 # Restart fluentd. Ideally we only need to restart if the password has changed from the original.
 # We can do this by causing a rolling update via adding a content hash label for cert and user secrets.
