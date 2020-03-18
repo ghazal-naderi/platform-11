864da92d9eb5a53c0bdfc8641474eaefab27a3eb
Calum Lacroix <c@cypher.cc>
2020-03-18 13:23:11 +0000

    linting now optional

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+LINT="${LINT:=yes}"
 set -eu -o pipefail
 
 # Start checks
@@ -21,18 +22,22 @@ for folder in k8s/*/; do
         # Generate k8s YAML for future parsing
         kubectl kustomize k8s >> "pkg/${file_name_clean}.yaml" && echo "✅ ${file_name_clean}: k8s Kustomize passes"
     fi
-    # KubEval check - we need to use git directly as @garethr hasn't updated the site yet
-    kubeval -v "${KUBERNETES_VERSION#?}" --ignore-missing-schemas -s "https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/" "pkg/${file_name_clean}.yaml" && echo "✅ ${file_name_clean}: k8s kubeval passes"
-    # Kube-score checks - we need to ignore the securityContext test as we're not using SELinux. We may add more to this.s
-    kube-score score --ignore-test container-security-context "pkg/${file_name_clean}.yaml" && echo "✅ ${file_name_clean}: k8s kube-score passes"
+    if [[ "${LINT}" == 'yes' ]]; then
+      # KubEval check - we need to use git directly as @garethr hasn't updated the site yet
+      kubeval -v "${KUBERNETES_VERSION#?}" --ignore-missing-schemas -s "https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/" "pkg/${file_name_clean}.yaml" && echo "✅ ${file_name_clean}: k8s kubeval passes"
+      # Kube-score checks - we need to ignore the securityContext test as we're not using SELinux. We may add more to this.s
+      kube-score score --ignore-test container-security-context "pkg/${file_name_clean}.yaml" && echo "✅ ${file_name_clean}: k8s kube-score passes"
+    fi
 done
 
 # tflint and terraform checks
 for folder in terraform/*/; do
     cwd="$(pwd)"
+    if [[ "${LINT}" == 'yes' ]]; then
     (cd "${folder}" && terraform init -backend=false && terraform validate) && echo "✅ terraform validate ${folder} passes"
-    tflint "${folder}" && echo "✅ terraform tflint ${folder} passes"
-    tfsec "${folder}" && echo "✅ terraform tfsec ${folder} passes"
+      tflint "${folder}" && echo "✅ terraform tflint ${folder} passes"
+      tfsec "${folder}" && echo "✅ terraform tfsec ${folder} passes"
+    fi
     mkdir -p "pkg/${folder}"
     cp -r "${cwd}/${folder}"* "pkg/${folder}"
 done
