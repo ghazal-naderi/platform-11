d590840592b745e587fcce0c1b56fcedbc5b9fe9
Calum Lacroix <c@cypher.cc>
2020-03-19 13:31:57 +0000

    Refactor client repo pipelines from feedback

--- components/client-repo-gather-deps/.github/workflows/gather.sh
+++ components/client-repo-gather-deps/.github/workflows/gather.sh
@@ -1,46 +1,46 @@
 #!/bin/bash -x 
 set -eu -o pipefail
-COUNT=$(( $(./yq r manifest.yaml --length repos) - 1 ))
+COUNT=$(( $(./yq r manifest.yaml --length packages) - 1 ))
+rm -rf temp/ || mkdir temp/
 git config user.email "bot@11fs.com"
 git config user.name "11:FS Bot"
-clone_repo() {
-            local rname=${1}
-            local rref=${2}
-            local rgithub=${3}
-            mkdir -p "structs"
-            rm -rf "structs/${rname}"
-            git clone -c advice.detachedHead=false --quiet --depth=1 --progress -b "${rref}" "https://${GITHUB_SECRET_TOKEN}@github.com/${rgithub}.git" "structs/${rname}"
-            rm -rf "structs/${rname}/.git"
+render_package() {
+            local pname=${1}
+            local pref=${2}
+            local path=${3}
+            mkdir -p "temp/${pname}"
+            rm -rf "${path}"
+            git clone -c advice.detachedHead=false --quiet --progress "https://${GITHUB_SECRET_TOKEN}@github.com/11FSConsulting/platform.git" "temp/${pname}"
+            (cd "temp/${pname}" && git checkout "${pref}")
+            rm -rf "temp/${pname}/.git"
+            (cd "temp/${pname}" && LINT=no ./build.sh) #docker run -e LINT=no -v "$(pwd):/workspace" -t platform/infra-tester)
+            if [[ "${path}" =~ k8s/.* ]]; then
+                git rm --cached "${path}.yaml" || mkdir -p "$(echo "${path}" | cut -d'/' -f1)" 
+                cp -r "temp/${pname}/pkg/${pname}"* "${path}.yaml"
+                git add "${path}.yaml"
+            else
+              git rm -r --cached "${path}" || mkdir -p "${path}"
+              cp -r "temp/${pname}/pkg/${pname}/" "${path}"
+              git add "${path}"
+            fi
+            rm -rf "temp/${pname}"
 }
 
-for repo in $(seq 0 "${COUNT}"); do
-    rname=$(./yq r manifest.yaml "repos[${repo}].name")
-    rgithub=$(./yq r manifest.yaml "repos[${repo}].github")
-    rref=$(./yq r manifest.yaml "repos[${repo}].ref")
-    clone_repo "${rname}" "${rref}" "${rgithub}"
-    if [ -f "structs/${rname}" ]; then 
-        if [ -f "structs/${rname}/build.sh" ]; then
-            (cd "structs/${rname}" && ./build.sh)
-        fi
-    fi
-done
-
-(cd structs && docker run -e LINT=no -v "$(pwd):/workspace" -t platform/infra-tester)
-mv structs/pkg .structs
-git rm --cached -r .structs
-for repo in $(seq 0 "${COUNT}"); do
-  rname=$(./yq r manifest.yaml "repos[${repo}].name")
-  scount=$(./yq r manifest.yaml --length "repos[${repo}].packages")
-  for struct in $(seq 0 "${scount}"); do
-    sname=$(./yq r manifest.yaml "repos[${repo}].packages[${struct}].name") 
-    stype=$(./yq r manifest.yaml "repos[${repo}].packages[${struct}].type") 
-    git add ".structs/${stype}/${sname}"*
-  done
+for package in $(seq 0 "${COUNT}"); do 
+    pname="$(./yq r manifest.yaml "packages[${package}].name")"
+    defaultref="master"
+    pref="$(./yq r manifest.yaml "packages[${package}].ref")"
+    pref="${pref:-$defaultref}"
+    path="$(./yq r manifest.yaml "packages[${package}].path")"
+    defaultpath="structs/${pname}"
+    path="${path:-$defaultpath}"
+    render_package "${pname}" "${pref}" "${path}"
 done
 
 if git commit -m 'Vendor dependencies'; then
   git remote set-url origin "https://${GITHUB_SECRET_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
   git push origin "HEAD:${GITHUB_HEAD_REF}"
+  echo "changes committed"
 else
   echo "no changes for commit"
 fi
