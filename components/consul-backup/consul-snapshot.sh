6ca768c1447bbd4cc93530e344ef3e9af04299dd
redscaresu <redscaresu@users.noreply.github.com>
2020-08-11 11:31:42 +0100

    Consul backup docker (#97)
    * the docker container for making consul backups
    
    * placeholder for secrets
    
    * add creds
    
    * create volume to do the things
    
    * save snapshots to volume

---
+++ components/consul-backup/consul-snapshot.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+CURRENT_TIME=$(date +"%FT%H%M%S")
+
+echo [default] > ~/.aws/credentials
+echo aws_access_key_id = $CONSUL_BUCKET_ACCESS >> ~/.aws/credentials
+echo aws_secret_access_key = $CONSUL_BUCKET_SECRET >> ~/.aws/credentials
+
+# consul snapshot save -http-addr=https://docker.for.mac.localhost:8500 ${CURRENT_TIME}.snap
+consul snapshot save -http-addr=$HTTP_ADDR /workspace/${CURRENT_TIME}.snap
+
+# aws s3 cp ${CURRENT_TIME}.snap s3://consul-prod-backup/${CURRENT_TIME}.snap --endpoint-url https://fr9ckauowgwh.compat.objectstorage.me-jeddah-1.o
+aws s3 cp /workspace/${CURRENT_TIME}.snap s3://consul-prod-backup/${CURRENT_TIME}.snap --endpoint-url $ENDPOINT_URL
