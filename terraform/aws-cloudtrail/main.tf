d0ccaffded12bb735acffae2b53dc6701dccd287
ghazal-naderi <61361506+ghazal-naderi@users.noreply.github.com>
2021-02-12 10:28:46 +0000

    edit permission on kms (#199)
    * edit permission on kms
    
    * added new principal to cloud trail permission on kms

--- terraform/aws-cloudtrail/main.tf
+++ terraform/aws-cloudtrail/main.tf
@@ -49,7 +49,36 @@ resource "aws_kms_key" "audit" {
       "Resource": "*"
     },
     {
-      "Sid": "Allow CloudTrail to encrypt logs",
+        "Sid": "Allow CloudTrail to encrypt logs",
+        "Effect": "Allow",
+        "Principal": {"Service": ["cloudtrail.amazonaws.com"]},
+        "Action": "kms:GenerateDataKey*",
+        "Resource": "*",
+        "Condition": {"StringLike": {"kms:EncryptionContext:aws:cloudtrail:arn": "arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"}}
+     },
+    {
+        "Sid": "Allow CloudTrail to describe key",
+        "Effect": "Allow",
+        "Principal": {"Service": ["cloudtrail.amazonaws.com"]},
+        "Action": "kms:DescribeKey",
+        "Resource": "*"
+    },
+    {
+        "Sid": "Allow principals in the account to decrypt log files",
+        "Effect": "Allow",
+        "Principal": {"AWS": "*"},
+        "Action": [
+            "kms:Decrypt",
+            "kms:ReEncryptFrom"
+        ],
+        "Resource": "*",
+        "Condition": {
+            "StringEquals": {"kms:CallerAccount": "${data.aws_caller_identity.current.account_id}"},
+            "StringLike": {"kms:EncryptionContext:aws:cloudtrail:arn": "arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"}
+      }
+    },
+    {
+      "Sid": "Allow log group to encrypt logs",
       "Effect": "Allow",
       "Principal": {
         "Service": "logs.${data.aws_region.current.name}.amazonaws.com"
@@ -65,7 +94,7 @@ resource "aws_kms_key" "audit" {
       "Condition": {
         "StringLike": {
           "kms:EncryptionContext:aws:cloudtrail:arn": [
-            "arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"
+            "arn:aws:logs:*:${data.aws_caller_identity.current.account_id}:log-group/*"
           ]
         }
       }
