a140e227f920aaf00d078eb9e12ee9b64fee2ebb
ghazal-naderi <61361506+ghazal-naderi@users.noreply.github.com>
2021-06-03 16:07:16 +0100

    removed duplicate s3 logging bucket (#273)

--- terraform/aws-cdn/main.tf
+++ terraform/aws-cdn/main.tf
@@ -31,21 +31,10 @@ data "aws_route53_zone" "parent" {
   name = "${var.zone}."
 }
 
-resource "aws_s3_bucket" "logs" {
+data "aws_s3_bucket" "logs" {
   bucket = "${var.project}-${var.environment}-logs"
-  acl    = "log-delivery-write"
-  versioning {
-    enabled = true
-  }
- server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.cnd-key.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
 }
+
 resource "aws_route53_record" "assets" {
   zone_id = data.aws_route53_zone.parent.zone_id
   name    = "${var.subdomain}.${var.environment}.${var.zone}"
@@ -170,7 +159,7 @@ resource "aws_s3_bucket" "b" {
     enabled = true
   }
   logging {
-    target_bucket = aws_s3_bucket.logs.id
+    target_bucket = data.aws_s3_bucket.logs.id
     target_prefix = "log/cdn/"
   }
  server_side_encryption_configuration {
